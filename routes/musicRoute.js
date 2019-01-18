36863f850c515da65b46634a26a89d67322b7a14
vinutama <muhammadkhevin@gmail.com>
2019-01-18 10:45:55 +0700

    server upload music

--- routes/musicRoute.js
+++ routes/musicRoute.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var musicController = require('../controllers/musicController')
+const { multer, sendUploadToGCS } = require('../helpers')
 
 router.get('/', musicController.getAll)
 router.get('/search', musicController.getAll)
 router.post('/lyric', musicController.getLyric)
-router.post('/', musicController.updload)
+router.post('/', multer.single('url'), sendUploadToGCS, musicController.updload)
 
 module.exports = router
