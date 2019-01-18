51f760b9b6dda137e0c5000c3224c985e7e43c0f
Ribka Haryuni <ribka.blue@gmail.com>
2019-01-18 09:05:08 +0700

    initial

--- models/User.js
+++ models/User.js
@@ -1,7 +1,37 @@
 const mongoose =require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-
+  email: {
+    type: String,
+    required: [true, 'email must be filled'],
+    validate: [{
+      validator: function(value) {
+        return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(value)
+      }, 
+      message: `Please enter a valid email`
+    }, { 
+      validator : function(value, cb) {
+        return new Promise( function(resolve, reject) {
+          User.findOne({ 
+            email : value 
+          }, function(err, data){
+            if(err) throw error
+            else{
+              if (data){
+                reject(false)
+              }else{
+                resolve(true)
+              }
+            }
+          })
+        })
+      },
+      message: 'Email already been used'
+    }]
+  },
+  password: {
+    type: String
+  }
 })
 
 const User = mongoose.model('User', UserSchema)
